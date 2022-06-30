b037b23d2a86612ca89060e91f95772ce0270934
Philippe R. Bretas <86687733+prbretas@users.noreply.github.com>
2022-06-30 17:00:24 -0300

    novas funcionalidades

--- donation.js
+++ donation.js
@@ -1,22 +1,49 @@
 //Estado da aplicação
-let numero = 5792.57
+let numero = 5280.54;
 
 //alteradores de estado
-function aumentar() {
-numero = numero + 20
-console.log(numero)
-mostrarNaTela()
+function aumentarIrme() {
+  numero = numero + 20;
+  console.log(numero);
+  mostrarNaTela();
+}
+
+function aumentarElasNoPoder() {
+  numero = numero + 20;
+  console.log(numero);
+  mostrarNaTela();
+}
+
+function aumentarFalaMulher() {
+  numero = numero + 20;
+  console.log(numero);
+  mostrarNaTela();
 }
 
 //mostrar no HTML a alteração
 
-  function mostrarNaTela(){
-const ValorArrecadado = document.getElementById("ValorArrecadado");
-ValorArrecadado.innerText = `R$ ${numero}`
-  }
-  
-  // iniciar
-  mostrarNaTela()
+function mostrarNaTela() {
+  const ValorArrecadado = document.getElementById("ValorArrecadado");
+  ValorArrecadado.innerText = `R$ ${numero}`;
+
+
+}
+
+// iniciar
+mostrarNaTela();
+
+
+
+
+let VAValue = ValorArrecadado.value
+
+
+
+
+
+
+
+
 
 
 
@@ -24,8 +51,7 @@ ValorArrecadado.innerText = `R$ ${numero}`
 
 /********************ANIMAÇÕES JS********************************/
 
-
-  //Lógica
+//Lógica
 
 // 1 - Selecionar elementos que devem ser animados
 // 2 - Definir a classe que é adcionada durante a animação
@@ -36,55 +62,45 @@ ValorArrecadado.innerText = `R$ ${numero}`
 // 4 - Ativar a função de animação toda vez que o usuário ultilizar o scroll
 // 5 - Otimizar ativação
 
-
-
 //DEBOUNCE DO LODASH
 
-const debounce = function(func, wait, immediate) {
+const debounce = function (func, wait, immediate) {
   let timeout;
-  return function(...args) {
-  const context = this;
-  const later = function() {
-  timeout = null;
-  if (!immediate) func.apply(context, args) ;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
     };
-  const callNow = immediate && !timeout;
-  clearTimeout(timeout);
-  timeout = setTimeout(later, wait);
-  if (callNow) func.apply (context, args);
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
   };
 };
 
-
-
-
-
-
-
-
-const target = document.querySelectorAll('[data-anime]')
-const animationClass = 'animate'
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
 
 function animeScroll() {
-  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4
+  const windowTop = window.pageYOffset + (window.innerHeight * 3) / 4;
 
   target.forEach(function (element) {
     if (windowTop > element.offsetTop) {
-      element.classList.add(animationClass)
+      element.classList.add(animationClass);
     } else {
-      element.classList.remove(animationClass)
+      element.classList.remove(animationClass);
     }
-  })
+  });
 }
 
 animeScroll();
 
-
-if(target.length) {
-
-window.addEventListener('scroll', debounce(function () {
-  animeScroll()
-}, 10));
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(function () {
+      animeScroll();
+    }, 10)
+  );
 }
-
-  
